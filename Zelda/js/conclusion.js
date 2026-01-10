904f096ae187b41c396afaf72c9594ff1a0363a2
eltitorodri <rbernalgomez@safareyes.es>
2026-01-10 11:27:03 +0100

    Enhancing the conclusion file

---
+++ Zelda/js/conclusion.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const counters = document.querySelectorAll('.award-number');
+    
+    counters.forEach(counter => {
+        const updateCount = () => {
+            const target = +counter.getAttribute('data-target');
+            const count = +counter.innerText;
+            const speed = 50; // A menor número, más rápido
+            
+            const inc = target / speed;
+
+            if (count < target) {
+                counter.innerText = Math.ceil(count + inc);
+                setTimeout(updateCount, 20);
+            } else {
+                counter.innerText = target;
+            }
+        };
+        updateCount();
+    });
+});
